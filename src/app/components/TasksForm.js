303d8c355c34253961d92ef61343bc36a6b8e7ce
Lautaro <lautaromoreno1998@live.com>
2022-12-21 11:48:48 -0300

    First commit
    First commit

---
+++ src/app/components/TasksForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addTask } from '../../features/tasks/taskSlice'
+import { v4 as uuid } from 'uuid'
+import { useNavigate } from 'react-router-dom'
+
+function TasksForm() {
+
+  const [task, setTask] = useState({
+    title:"",
+    description: "",
+  })
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleChange = e =>{
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    dispatch(addTask({
+      ...task,
+      id: uuid(),
+    }))
+    navigate("/")
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name='title' type="text" placeholder='title' onChange={handleChange} />
+      <textarea name='description' placeholder='description' onChange={handleChange}></textarea>
+      <button>Save</button>
+    </form>
+  )
+}
+
+export default TasksForm
